1071d129b7357f1f1fcd30311c4f84456975ed2a
OPM-MASTER <96420420+OPM-MASTER@users.noreply.github.com>
2022-01-04 08:05:31 +0700

    update visitor.js

--- assets/js/visitor.js
+++ assets/js/visitor.js
@@ -1,6 +1,6 @@
 function getvisitor(){
 var xhr = new XMLHttpRequest();
-var url = 'https://api.countapi.xyz/hit/api-xfar05.herokuapp.com./visits';
+var url = 'https://api.countapi.xyz/hit/api-fongsi.herokuapp.com./visits';
 xhr.onloadend = function(){
 data = JSON.parse(this.responseText);
 document.getElementById("visitor").textContent = data.value
